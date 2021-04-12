3023a84327dd06dc3fea3a5c1c2e4309f7de66d9
shmkhaled <shmkhaled@gmail.com>
2021-04-12 14:34:30 +0200

    similarity measures

--- src/main/scala/GetSimilarity.scala
+++ src/main/scala/GetSimilarity.scala
@@ -1,8 +1,10 @@
+import com.github.vickumar1981.stringdistance.StringDistance.{Cosine, Tversky}
+import com.rockymadden.stringmetric.phonetic.SoundexMetric
+import com.rockymadden.stringmetric.similarity._
 import edu.cmu.lti.lexical_db.NictWordNet
 import edu.cmu.lti.ws4j.impl.Path
 import edu.cmu.lti.ws4j.util.WS4JConfiguration
-import info.debatty.java.stringsimilarity.Jaccard
-import info.debatty.java.stringsimilarity._
+import me.xdrop.fuzzywuzzy.FuzzySearch
 /*
 * Created by Shimaa 17.02.2021
 * */
@@ -21,30 +23,131 @@ class GetSimilarity extends Serializable{
 //      sim = (this.sentenceSimilarity(sent1,sent2)+this.sentenceSimilarity(sent2,sent1))/2
     sim
   }
-
+  //############################# Sequence-based measures ########################################
   /**
-    * Get the Jaccard similarity between two strings.*/
-  def getJaccardStringSimilarity(s1: String, s2: String): Double={
-    val j = new Jaccard(3)
-    var jaccardSim = j.similarity(s1, s2)
-    jaccardSim
+    * Get the Jaro similarity between two strings.*/
+  def getJaroStringSimilarity(s1: String, s2: String): Double={
+    val jaroSim = JaroMetric.compare(s1, s2).head
+    jaroSim
   }
-
+  /**
+    * Get the Jaro-Winkler similarity between two strings.*/
+  def getJaroWinklerStringSimilarity(s1: String, s2: String): Double={
+    val jaroWiinklerSim = JaroWinklerMetric.compare(s1, s2).head
+    jaroWiinklerSim
+  }
   /**
     * Get the Levenshtein similarity between two strings.*/
   def getLevenshteinStringSimilarity(s1: String, s2: String): Double={
-    val l = new Levenshtein()
-    var levenshteinSim = l.distance(s1, s2)
+    val levenshteinSim = LevenshteinMetric.compare(s1, s2).head
     levenshteinSim
   }
-
   /**
-    * Get the  Jaro-Winkler similarity between two strings.*/
-  def getJaroWinklerStringSimilarity(s1: String, s2: String): Double={
-    val jw = new JaroWinkler()
-    var JaroWinklerSim = jw.similarity(s1, s2)
-    JaroWinklerSim
+    * Get Hamming distance between two strings.*/
+  def getHammingDistanceSimilarity(s1: String, s2: String): Double={
+    val hammDistSim = HammingMetric.compare(s1, s2).head
+    hammDistSim
   }
+  /**
+    * Get ratio similarity between two strings.*/
+  def getRatioSimilarity(s1: String, s2: String): Double={
+    val ratioSim = FuzzySearch.ratio(s1, s2)
+    ratioSim
+  }
+  /**
+    * Get partial ratio similarity between two strings.*/
+  def getPartialRatioSimilarity(s1: String, s2: String): Double={
+    val partialRatioSim = FuzzySearch.partialRatio(s1, s2)
+    partialRatioSim
+  }
+  /**
+    * Get partial token sort similarity between two strings.*/
+  def getPartialTokenSortSimilarity(s1: String, s2: String): Double={
+    val partialTokenSortSim = FuzzySearch.tokenSortPartialRatio(s1, s2)
+    partialTokenSortSim
+  }
+  /**
+    * Get token sort similarity between two strings.*/
+  def getTokenSortSimilarity(s1: String, s2: String): Double={
+    val tokenSortSim = FuzzySearch.tokenSortRatio(s1, s2)
+    tokenSortSim
+  }
+
+  //############################# Token-based measures ########################################
+  /**
+    * Get cosine similarity between two strings.*/
+  def getCosineSimilarity(s1: String, s2: String): Double={
+    val cosineSim = Cosine.score(s1, s2)
+    cosineSim
+  }
+  /**
+    * Get Dice similarity between two strings.*/
+  def getDiceSimilarity(s1: String, s2: String): Double={
+    val diceSim = DiceSorensenMetric(1).compare(s1, s2).head
+    diceSim
+  }
+  /**
+    * Get the Jaccard similarity between two strings.*/
+  def getJaccardStringSimilarity(s1: String, s2: String): Double={
+//    val j = new Jaccard(3)
+//    val jaccardSim = j.similarity(s1, s2)
+    val jaccardSim = JaccardMetric(1).compare(s1, s2).head
+    jaccardSim
+  }
+  /**
+    * Get the Overlap Coefficient similarity between two strings.*/
+  def getOverlapCoefficientStringSimilarity(s1: String, s2: String): Double={
+    val overlapSim = OverlapMetric(1).compare(s1, s2).head
+    overlapSim
+  }
+  /**
+    * Get Tversky similarity between two strings.*/
+  def getTverskyStringSimilarity(s1: String, s2: String): Double={
+    val tverskySim = Tversky.score(s1, s2, 0.5)
+    tverskySim
+  }
+  // TfIdf
+
+  //############################## Hybrid-based measures ##############################
+  /*
+  * GeneralizedJaccard
+  MongeElkan
+  SoftTfIdf
+  */
+
+  //############################## Phonetic-based measures ##############################
+  /**
+    * Get Soundex phonetic similarity between two strings.*/
+  def getSoundexSimilarity(s1: String, s2: String): Boolean={
+    val soundexSim= SoundexMetric.compare(s1, s2).head
+    soundexSim
+  }
+
+
+
+//  /**
+//    * Get the Jaccard similarity between two strings.*/
+//  def getJaccardStringSimilarity(s1: String, s2: String): Double={
+//    val j = new Jaccard(3)
+//    var jaccardSim = j.similarity(s1, s2)
+//    jaccardSim
+//  }
+//
+//  /**
+//    * Get the Levenshtein similarity between two strings.*/
+//  def getLevenshteinStringSimilarity(s1: String, s2: String): Double={
+//    val l = new Levenshtein()
+//    var levenshteinSim = l.distance(s1, s2)
+//    levenshteinSim
+//  }
+//
+//  /**
+//    * Get the  Jaro-Winkler similarity between two strings.*/
+//  def getJaroWinklerStringSimilarity(s1: String, s2: String): Double={
+//    val jw = new JaroWinkler()
+//    var JaroWinklerSim = jw.similarity(s1, s2)
+//    JaroWinklerSim
+//  }
 //  def getStringSimilarity(s1: String, s2: String): Double={
 //    //    val cos = new Cosine(2)
 //    //    var cosSim = cos.similarity(s1, s2)
